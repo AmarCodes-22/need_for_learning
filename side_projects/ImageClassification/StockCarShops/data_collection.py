e43aabf67c84f3b40366e77e3d58f7b3d38a1799
Amar Pratap Singh <amarpratap2208@gmail.com>
2021-07-17 20:04:26 +0530

    Added data_collection.py for StockCarShops ImageClassification and deleted data_collection.ipynb, plan.txt

---
+++ side_projects/ImageClassification/StockCarShops/data_collection.py
@@ -0,0 +1,60 @@
+import os
+from pathlib import Path
+from natsort import natsorted
+import shutil
+import cv2 as cv
+
+#* Setting up paths
+current_working_dir = os.getcwd()
+main_project_path = str(Path(current_working_dir).parents[2])
+side_project_path = os.path.join(main_project_path, 
+                                'side_projects',
+                                'ImageClassification',
+                                'StockCarShops')
+data_dumps_path = os.path.join(main_project_path, 'side_projects', 'data_dumps')
+project_data_path = os.path.join(side_project_path,'data')
+project_data_shutil_path = os.path.join(side_project_path,'data_shutil')
+project_data_dump_path = os.path.join(data_dumps_path, 'ImageClassification', 'StockCarShops')
+
+paths = {
+    'main_project': main_project_path,
+    'side_project': side_project_path,
+    'data_dumps': data_dumps_path,
+    'project_data': project_data_path,
+    'project_data_dump': project_data_dump_path,
+    'project_data_shutil': project_data_shutil_path
+}
+
+#* Setting up the files in the datadump
+files = []
+for file_name in os.listdir(paths['project_data_dump']):
+    files.append(file_name)
+
+files = natsorted(files)
+
+#* Class names inferred from the directory structure
+class_names = []
+for dirpath, dirnames, filenames in os.walk(paths['project_data']):
+    class_names = dirnames
+    break
+
+#* Done only once
+# for i, file_name in enumerate(files):
+#     src_path = os.path.join(paths['project_data_dump'], file_name)
+#     dirname = os.path.join(paths['project_data_shutil'], class_names[i//25])
+    
+#     if not os.path.exists(paths['project_data_shutil']):
+#         os.system(f'mkdir {project_data_shutil_path}')
+#     if not os.path.exists(dirname):
+#         os.system(f'mkdir {dirname}')
+
+#     dst_path = os.path.join(dirname, file_name)
+
+#     shutil.copy(src_path, dst_path)
+
+
+
+
+
+
+
