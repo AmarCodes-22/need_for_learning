66e58e135b651f04c68dfcfe524c3a8aa727a52c
Amar Pratap Singh <amarpratap2208@gmail.com>
2021-07-20 13:22:24 +0530

    Collected highway footage for lane detection

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ count = 0
 while True:
     screenshot = game_screen.get_screenshot()
     cv.imshow('Game screen', screenshot)
+
     if count % 25 == 0:
         print('FPS: {}'.format(1/(time()-looptime)))
         count = 0
     looptime = time()
     count += 1
-
 
     if cv.waitKey(1) == ord('q'):
         break
