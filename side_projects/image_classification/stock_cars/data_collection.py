32717e87b0c25f6d49dff76b5f5b6de9cdb204ec
Amar Pratap Singh <amarpratap2208@gmail.com>
2021-07-21 20:47:36 +0530

    Meh

--- side_projects/image_classification/stock_cars/data_collection.py
+++ side_projects/image_classification/stock_cars/data_collection.py
@@ -10,10 +10,10 @@ current_working_dir = os.getcwd()
 main_project_path = str(Path(current_working_dir).parents[2])
 side_project_path = os.path.join(main_project_path, 
                                 'side_projects',
-                                'ImageClassification',
-                                'StockCarShops')
+                                'image_classification',
+                                'stock_cars')
 data_dumps_path = os.path.join(main_project_path, 'side_projects', 'data_dumps')
-project_data_path = os.path.join(side_project_path,'data')
+project_data_path = os.path.join(side_project_path,'new_data')
 project_data_shutil_path = os.path.join(side_project_path,'data_shutil')
 project_data_dump_path = os.path.join(data_dumps_path, 'ImageClassification', 'StockCarShops')
 
@@ -33,11 +33,22 @@ for file_name in os.listdir(paths['project_data_dump']):
 
 files = natsorted(files)
 
-#* Class names inferred from the directory structure(you have to make the directory structure first)
-class_names = []
+#* Class names
+class_names = ['gallardo', 'gto', 'clio', 'audi_a4', 'cobalt', 'supra', 'elise', 'audi_a3', 
+               'sl500', 'is_300', 'monaro', 'audi_tt', 'db9', 'murcielago', 'clk500', 'rx7', 
+               'ford_gt', 'viper', 'mercedes_benz', 'corvette', 'turbo', 'golf_gti', 'carrera_gt', 'cts', 
+               'mustang_gt', 'eclipse', 'carrera', 'cayman', 'evolution', 'wrx', 'punto', 'rx8']
+
+
 for dirpath, dirnames, filenames in os.walk(paths['project_data']):
-    class_names = dirnames
-    break
+    print('There are {} directories, {} files in {}'.format(len(dirnames), len(filenames), dirpath))
+
+print(files)
+
+#     class_names = dirnames
+#     break
+
+# print(class_names)
 
 #* Done only once(Temporary show for using shutil)
 # for i, file_name in enumerate(files):
@@ -52,7 +63,7 @@ for dirpath, dirnames, filenames in os.walk(paths['project_data']):
 
 #* Cropping the data to contain only the game screen
 #* Done only once
-# for dirpath, dirnames, filenames in os.walk(paths['project_data']):
+# for dirpath, dirnames, filenames in os.walk(paths['project_data'])
 #     if len(filenames) > 0:
 #         for filename in filenames:
 #             filepath = os.path.join(dirpath, filename)
@@ -80,5 +91,5 @@ for dirpath, dirnames, filenames in os.walk(paths['project_data']):
 #                 dst_path = os.path.join(train_dir_path, file)
 #                 os.system('mv {} {}'.format(src_path, dst_path))
 
-for dirpath, dirnames, filenames in os.walk(paths['project_data']):
-    print('There are {} directories, {} files in {}'.format(len(dirnames), len(filenames), dirpath))
+# for dirpath, dirnames, filenames in os.walk(paths['project_data']):
+#     print('There are {} directories, {} files in {}'.format(len(dirnames), len(filenames), dirpath))
