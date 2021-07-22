6797a071d893f20bd83fd27717226835e65008a5
Amar Pratap Singh <amarpratap2208@gmail.com>
2021-07-22 14:21:30 +0530

    Collected every 50th frame for hsv thresholding check from highway footage tpp

--- data_collection.py
+++ data_collection.py
@@ -2,32 +2,50 @@ import os
 import cv2 as cv
 import numpy as np
 from screen_capture import ScreenCapture
+import uuid
 
 project_dir_path = os.getcwd()
 highway_video_path = os.path.join(project_dir_path, 'data', 'videos', 'highway_footage_tpp.avi')
+images_path = os.path.join(project_dir_path, 'data', 'images')
 
-game_screen = ScreenCapture()
-screen_dim = game_screen.window_dim
-print(screen_dim)
+#* Image capture for hsv thresholding using prerecorded game footage
+# count = 0
+# cap = cv.VideoCapture(highway_video_path)
+# while True:
+#     ret, frame = cap.read()
 
-out = cv.VideoWriter(highway_video_path, 
-                     cv.VideoWriter_fourcc('M','J','P','G'), 
-                     60, 
-                     (screen_dim[2], screen_dim[3]))
+#     if count % 50 == 0:
+#         cv.imshow('frame to be saved', frame)
+#         file_name = os.path.join(images_path, str(uuid.uuid4())+ '.jpg')
+#         cv.imwrite(file_name, frame)
+#         count = 0
+#     count += 1
 
-count = 0
-while True:
-    screenshot = game_screen.get_screenshot()
-    out.write(screenshot)
-    # cv.imshow('Test', screenshot)
-
-    if count % 50 == 0:
-        print('Recording')
-        count = 0
-    count += 1
-
-    if cv.waitKey(1) == ord('q'):
-        break
+#     if cv.waitKey(1) == ord('q'):
+#         break
 
 
-out.release()
+#* Video footage capture
+# game_screen = ScreenCapture()
+# screen_dim = game_screen.window_dim
+# print(screen_dim)
+
+# out = cv.VideoWriter(highway_video_path, 
+#                      cv.VideoWriter_fourcc('M','J','P','G'), 
+#                      60, 
+#                      (screen_dim[2], screen_dim[3]))
+# count = 0
+# while True:
+#     screenshot = game_screen.get_screenshot()
+#     out.write(screenshot)
+#     cv.imshow('Test', screenshot)
+
+#     if count % 50 == 0:
+#         print('Recording')
+#         count = 0
+#     count += 1
+
+#     if cv.waitKey(1) == ord('q'):
+#         break
+
+# out.release()
