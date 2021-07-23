f987a2d8e4aa3c8e1811a1dd3840c0ae731775bd
Amar Pratap Singh <amarpratap2208@gmail.com>
2021-07-23 18:22:14 +0530

    Using custom filter for vertical edge detection instead of canny and using hough transform for line detection with constrained slopes and biases.

--- lane_detection.py
+++ lane_detection.py
@@ -1,33 +1,68 @@
 import os
 import cv2 as cv
 import numpy as np
+from numpy.lib.function_base import blackman
 from hsv_filter import HSVFilter
 
 hsv_filter = HSVFilter(0,0,100,30,255,255)
-# input_video_path = os.path.join(os.getcwd(), 'data', 'videos', 'highway_footage_tpp.avi')
-vertices = np.array([[0,480], [0,240], [80,200], [560,200], [640,240], [640,480], [480,480], [375,275], [275,275], [160,480]])
-# cap = cv.VideoCapture(input_video_path)
 
-def roi(frame, vertices):
-    b,g,r = cv.split(frame)
-
-    mask = np.ones_like(b)
-    cv.fillPoly(mask, vertices, 255)
-
-    masked_b = cv.bitwise_and(b, mask)
-    masked_g = cv.bitwise_and(g, mask)
-    masked_r = cv.bitwise_and(r, mask)
-    masked = cv.merge([masked_b, masked_g, masked_r])
-
-    return masked
+vertices = np.array([[0,480], [0,240], [80,200], [560,200], [640,240], [640,480]]) 
+                    #  [480,480], [375,275], [275,275], [160,480]])
 
 def preprocess(original_image):
-    processed_image = cv.GaussianBlur(original_image, (7,7), 0)
-    processed_image = cv.Canny(processed_image, 100, 200)
+    processed_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+
+    #* Custom edge detector
+    kernel = np.array([[1,0,-1],
+                       [1,0,-1],
+                       [1,0,-1]])
+
+    processed_image = cv.filter2D(processed_image, -1, kernel)
+    _, processed_image = cv.threshold(processed_image, 25, 200, cv.THRESH_BINARY)
+
+    
+    #* Dilation and erosion
+    # erode_kernel = np.ones((3,3),np.uint8)
+    # dilation_kernel = np.ones((3,3),np.uint8)
+    # processed_image = cv.dilate(processed_image,dilation_kernel,iterations = 1)
+    # processed_image = cv.erode(processed_image,erode_kernel,iterations = 1)
+
+    # processed_image = cv.GaussianBlur(processed_image, (3,3), 0)
+    # processed_image = cv.Canny(processed_image, 150, 250)
+
     return processed_image
 
-def get_lanes(frame):
-    frame = roi(frame, [vertices])
-    frame = hsv_filter.apply_filter(frame)
-    frame = preprocess(frame)
+def roi(frame, vertices):
+    mask = np.ones_like(frame)
+    cv.fillPoly(mask, vertices, 255)
+    masked = cv.bitwise_and(frame, mask)
+    return masked
+
+def draw_lines(frame, lines):
+    blank = np.zeros_like(frame)
+    try:
+        for line in lines:
+            coords = line[0]
+            x1, y1, x2, y2 = coords
+            angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
+            # print(coords, angle, type(angle))
+            if 30 < abs(angle) < 60 and max(y1, y2) > 350:
+                cv.line(frame, (x1, y1), (x2, y2), (255,255,255), 4, lineType=cv.LINE_4)
+            # break
+
+        return frame
+    except: 
+        return frame
+
+def hough_lines(frame):
+    lines = cv.HoughLinesP(frame, 1, np.pi/180, 50, minLineLength=50,maxLineGap=10)
+    # print(lines)
+    frame = draw_lines(frame, lines)
     return frame
+
+def get_edges(frame):
+    frame = preprocess(frame)
+    frame = roi(frame, [vertices])
+    frame = hough_lines(frame)
+    return frame
+
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from time import time
+import os
+import time
 import cv2 as cv
 from screen_capture import ScreenCapture
-from lane_detection import get_lanes
+from lane_detection import get_edges
 
-game_screen = ScreenCapture()
-looptime = time()
+# game_screen = ScreenCapture()
+looptime = time.time()
 count = 0
 
+input_video_path = os.path.join(os.getcwd(), 'data', 'videos', 'highway_footage_tpp.avi')
+cap = cv.VideoCapture(input_video_path)
+
 while True:
-    screenshot = game_screen.get_screenshot()
-    lanes = get_lanes(screenshot)
+    #* For live game footage
+    # screenshot = game_screen.get_screenshot()
+    # lanes = get_edges(screenshot)
+    # cv.imshow('Lanes', lanes)
+
+    #* For prerecorded videos
+    ret, frame = cap.read()
+    lanes = get_edges(frame)
     cv.imshow('Lanes', lanes)
+    # cv.waitKey(0)
+    # break
 
     if count % 25 == 0:
-        print('FPS: {}'.format(1/(time()-looptime)))
+        print('FPS: {}'.format(1/(time.time()-looptime)))
         count = 0
-    looptime = time()
+    looptime = time.time()
     count += 1
 
     if cv.waitKey(1) == ord('q'):
