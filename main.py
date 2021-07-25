1246f8a972871cd06ea301c0808d051d1115208c
Amar Pratap Singh <amarpratap2208@gmail.com>
2021-07-25 21:36:28 +0530

    meh

--- main.py
+++ main.py
@@ -17,19 +17,19 @@ cap = cv.VideoCapture(input_video_path)
 # out = cv.VideoWriter(output_video_path,cv.VideoWriter_fourcc('M','J','P','G'), 10, (640,480))
 
 while True:
-    #* For live game footage
+    # * For live game footage
     # screenshot = game_screen.get_screenshot()
     # lanes = get_lanes(screenshot)
     # cv.imshow('Lanes', lanes)
 
-    #* For prerecorded videos
+    # * For prerecorded videos
     ret, frame = cap.read()
     lanes = lane_detector.get_lanes(frame)
     # out.write(lanes)
     cv.imshow('Lanes', lanes)
 
     if count % 25 == 0:
-        print('FPS: {}'.format(1/(time.time()-looptime)))
+        print('FPS: {}'.format(1 / (time.time() - looptime)))
         count = 0
     looptime = time.time()
     count += 1
